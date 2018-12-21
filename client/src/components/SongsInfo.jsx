c5610f34eb0d0b008dfd4d59c127bf52a5809cb7
Steven S <underyourcover@icloud.com>
2018-12-20 19:01:32 -0800

    client test code wasn't added during previous commit

---
+++ client/src/components/SongsInfo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class SongsInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <div> 'Hello' </div>
+      </div>
+    );
+  }
+}
+export default SongsInfo;
---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SongsInfo from './components/SongsInfo.jsx';
+
+ReactDOM.render(<SongsInfo />, document.getElementById('app'));
